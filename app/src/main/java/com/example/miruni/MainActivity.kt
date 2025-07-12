13259cfa96fe9445ac9a31661824dab75418d45e
moon890309 <abcdefg8989@naver.com>
2025-07-12 14:27:43 +0900

    [FEAT]구현 확인 임시 이동 #37

--- app/src/main/java/com/example/miruni/MainActivity.kt
+++ app/src/main/java/com/example/miruni/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.miruni
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.miruni.databinding.ActivityMainBinding
 
@@ -15,6 +20,18 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                    1000
+                )
+            }
+        }
 
         supportFragmentManager.beginTransaction()
             .replace(R.id.main_frm, CalendarFragment())
