37d91f40f1885651db37a6413f7c370d8f1f4ecc
moon890309 <abcdefg8989@naver.com>
2025-08-02 02:32:06 +0900

    [FEAT]Merge 병합 과정 수정 및 홈페이지 기능 구현 #83

--- app/src/main/java/com/example/miruni/APIService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.miruni
-
-import retrofit2.http.GET
-import retrofit2.http.Header
-
-interface APIService {
-    @GET("api/homePage")
-    suspend fun getHomepage(@Header("Authorization") token: String) : HomepageResponse
-}
